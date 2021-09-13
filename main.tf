5627f599d89390df91b38e4afe9a6e057a01b649
root <root@ip-172-31-4-158.us-east-2.compute.internal>
2021-09-13 12:38:18 +0000

    commiting main.tf file from local

---
+++ main.tf
@@ -0,0 +1,110 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_vpc" "default" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "tf_test"
+  }
+}
+
+resource "aws_subnet" "tf_test_subnet" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "tf_test_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.default.id
+
+  tags = {
+    Name = "tf_test_ig"
+  }
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "aws_route_table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.tf_test_subnet.id
+  route_table_id = aws_route_table.r.id
+}
+
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "instance_sg"
+  description = "Used in the terraform"
+  vpc_id      = aws_vpc.default.id
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+#resource "aws_instance" "web" {
+#  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+#  ami = var.aws_amis[var.aws_region]
+
+  # The name of our SSH keypair you've created and downloaded
+  # from the AWS console.
+  #
+  # https://console.aws.amazon.com/ec2/v2/home?region=us-west-2#KeyPairs:
+  #
+#  key_name = var.key_name
+
+  # Our Security group to allow HTTP and SSH access
+#  vpc_security_group_ids = [aws_security_group.default.id]
+#  subnet_id              = aws_subnet.tf_test_subnet.id
+
+  #Instance tags
+
+#  tags = {
+#    Name = "elb-example"
+#  }
+#}
